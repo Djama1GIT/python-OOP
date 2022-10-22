07b769b84f96f4ad1b68d155c4388690f962bb80
Dj.ama1 <dj.ama1@mail.ru>
2022-10-22 21:13:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class MagicMethods:
+    ...
+
+
+class Cat:
+    def __init__(self, breed, name, age):
+        self.breed = breed
+        self.name = name
+        self.age = age
+
+
+cat = Cat('idk', 'Alex', 3)
